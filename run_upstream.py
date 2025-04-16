c388cd0c0c701ae5510e6562bcb4bdb1a79e087e
raojun1023 <raojun1023@163.com>
2025-04-16 16:39:46 +0800

    major-20250409-add-salmon

--- run_upstream.py
+++ run_upstream.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from software.up_software import *
 
 
@@ -7,31 +8,30 @@ def parameter_input():
     parser.add_argument('input1', help='Path to the fastq(.gz) file of read1')
     parser.add_argument('input2', help='Path to the fastq(.gz) file of read2')
     parser.add_argument('--host',
-                        help='Name(s) of Bowtie2 index(es) built before, please separate different names with "," when removing more than one hosts (for example: "human,mouse") (default: None, which means this step will be skipped)',
+                        help='Name(s) of Bowtie2 index(es) (default: None, skip host removal)',
                         default=None)
-    parser.add_argument('-a', '--adapter', type=int,
-                        help='Choose the adapter file used by trimmomatic to remove adapter sequences (please input the interger code, 0:NexteraPE-PE, 1:TruSeq3-PE-2, 2:TruSeq3-PE, 3:TruSeq2-PE, default:NexteraPE-PE)',
+    parser.add_argument('-a', '--adapter', type=int, choices=[0, 1, 2, 3],
+                        help='Adapter file code for Trimmomatic (0:NexteraPE-PE, 1:TruSeq3-PE-2, 2:TruSeq3-PE, 3:TruSeq2-PE)',
                         default=0)
-    parser.add_argument('-o', '--output', help='Path to output directory', default=f'{os.getcwd()}/result')
-    parser.add_argument('-t', '--threads', type=int, help='Threads used to run this pipeline (default:1)', default=1)
+    parser.add_argument('-o', '--output', help='Output directory path', default=os.path.join(os.getcwd(), 'result'))
+    parser.add_argument('-t', '--threads', type=int, help='Number of threads (default:1)', default=1)
     parser.add_argument('-r', '--remove_inter_result', action='store_true',
-                        help='Remove intermediate results (use carefully, this will only keep the final viral contigs, fastqc and viromeqc reports!)',
+                        help='Remove intermediate results (keeps final contigs)',
                         default=False)
     parser.add_argument('-k', '--keep_log', action='store_true',
-                        help='This parameter allows you to continue a killed run, but please make sure you have not changed any files in the output directory.',
+                        help='Resume interrupted run (do not modify output dir)',
                         default=False)
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    db = "/cpfs01/projects-HDD/cfff-47998b01bebd_HDD/rj_24212030018/db"  # please adjust db here
+    db = "/cpfs01/projects-HDD/cfff-47998b01bebd_HDD/rj_24212030018/db"  # 硬编码路径，后续改为配置参数
     args = parameter_input()
     threads = args.threads
     output = args.output
-    sample1 = get_sample_name(args.input1.split('/')[-1])
-    sample2 = get_sample_name(args.input2.split('/')[-1])
-    sample = sample1[0: -2]
+    sample1 = get_sample_name(os.path.basename(args.input1))
+    sample2 = get_sample_name(os.path.basename(args.input2))
+    sample = sample1[:-2]  # 假设样本命名规范可靠
 
     # create the output directory if it does not exist
     create_output_file(output)
@@ -41,110 +41,104 @@ if __name__ == '__main__':
         with open(f"{output}/{sample}log.txt", "w") as f:
             f.write("0\n")
 
-    # get log info
-    with open(f"{output}/{sample}log.txt", "r") as f:
-        log = int(f.readline()[0: -1])
+    log_path = os.path.join(output, f"{sample}log.txt")
+    if not args.keep_log:
+        with open(log_path, "w") as f:
+            f.write("0\n")
 
-    # assess quality of sequencing with fastqc
+    # 读取日志状态
+    with open(log_path, "r") as f:
+        log = int(f.readline().strip())
+
+    # 步骤1: FastQC质量评估
     if log < 1:
         run_fastqc(output, args.input1, args.input2, threads)
         log = 1
-        with open(f"{output}/{sample}log.txt", "w") as f:
+        with open(log_path, "w") as f:
             f.write(f"{log}\n")
 
-            # trim adapters with trimmomatic
+    # 步骤2: Trimmomatic去接头
     if log < 2:
-        if args.adapter == 1:
-            adapter = f"{db}/adapters/TruSeq3-PE-2.fa"
-        elif args.adapter == 2:
-            adapter = f"{db}/adapters/TruSeq3-PE.fa"
-        elif args.adapter == 3:
-            adapter = f"{db}/adapters/TruSeq2-PE.fa"
-        else:
-            adapter = f"{db}/adapters/NexteraPE-PE.fa"
-        run_trim(output, threads, args.input1, args.input2, sample1, sample2, adapter)
+        adapter_map = {
+            1: "TruSeq3-PE-2.fa",
+            2: "TruSeq3-PE.fa",
+            3: "TruSeq2-PE.fa",
+        }
+        adapter_file = adapter_map.get(args.adapter, "NexteraPE-PE.fa")
+        adapter_path = os.path.join(db, "adapters", adapter_file)
+        run_trim(output, threads, args.input1, args.input2, sample1, sample2, adapter_path)
         log = 2
-        with open(f"{output}/{sample}log.txt", "w") as f:
+        with open(log_path, "w") as f:
             f.write(f"{log}\n")
 
-            # trim host sequences with bowtie2
+    # 步骤3: Bowtie2去宿主序列
     if log < 3:
-        if args.host is not None:
+        if args.host:
             host_list = args.host.split(',')
+            index_path = [os.path.join(db, "bowtie2_index", na, na) for na in host_list]
+            run_bowtie2(output, threads, sample1, sample2, index_path, sample)
         else:
-            host_list = []
-        index_path = []
-        for na in host_list:
-            index_path.append(f"{db}/bowtie2_index/{na}/{na}")
-        run_bowtie2(output, threads, sample1, sample2, index_path, sample)
+            print("Skipping host removal: No host index provided.")
         log = 3
-        with open(f"{output}/{sample}log.txt", "w") as f:
+        with open(log_path, "w") as f:
             f.write(f"{log}\n")
 
-            # report the data quality after do QCs with viromeQC
+    # 4. assemble contigs with spades
     if log < 4:
-        run_viromeQC(output, sample1, sample2)
+        run_spades(output, threads, sample1, sample2, sample)
         log = 4
         with open(f"{output}/{sample}log.txt", "w") as f:
             f.write(f"{log}\n")
 
-            # assemble contigs with spades
+    # 5. trim short contigs with vsearch
     if log < 5:
-        run_spades(output, threads, sample1, sample2, sample)
+        run_vsearch_1(output, sample, threads)
         log = 5
         with open(f"{output}/{sample}log.txt", "w") as f:
             f.write(f"{log}\n")
 
-            # trim short contigs with vsearch
+    # 6.find viral contigs with virsorter
     if log < 6:
-        run_vsearch_1(output, sample, threads)
+        run_virsorter(output, threads, sample)
         log = 6
+        with open(log_path, "w") as f:
+            f.write(f"{log}\n")
+
+    # 7.find viral contigs by comparing contigs to databases with blastn
+    if log < 7:
+        run_blastn(output, threads, sample)
+        log = 7
         with open(f"{output}/{sample}log.txt", "w") as f:
             f.write(f"{log}\n")
 
-            # find viral contigs with virsorter
-    if log < 7:
-        log = 7
-        run_virsorter(output, threads, sample)
-    with open(f"{output}/{sample}log.txt", "w") as f:
-        f.write(f"{log}\n")
-
-        # find viral contigs by comparing contigs to databases with blastn
+    # 8.filter blastn results and integrate them with virsorter results
     if log < 8:
-        run_blastn(output, threads, sample)
+        run_combination(output, sample)
         log = 8
         with open(f"{output}/{sample}log.txt", "w") as f:
             f.write(f"{log}\n")
 
-            # filter blastn results and integrate them with virsorter results
-    print("all down break")
+            # drop viral contigs with low quality or low completeness with checkv
     if log < 9:
-        run_combination(output, sample)
+        run_checkv(output, threads, sample)
         log = 9
         with open(f"{output}/{sample}log.txt", "w") as f:
             f.write(f"{log}\n")
 
-            # drop viral contigs with low quality or low completeness with checkv
     if log < 10:
-        run_checkv(output, threads, sample)
+        high_quality_output(output, sample)
         log = 10
         with open(f"{output}/{sample}log.txt", "w") as f:
             f.write(f"{log}\n")
 
+            # cluster contigs and get final non-redundant contigs
     if log < 11:
-        high_quality_output(output, sample)
+        run_vsearch_2(output, threads, sample)
         log = 11
         with open(f"{output}/{sample}log.txt", "w") as f:
             f.write(f"{log}\n")
 
-            # cluster contigs and get final non-redundant contigs
-    if log < 12:
-        run_vsearch_2(output, threads, sample)
-        log = 12
-        with open(f"{output}/{sample}log.txt", "w") as f:
-            f.write(f"{log}\n")
-
     print("all steps finished")
-
-    if args.remove_inter_result == True:
+    if args.remove_inter_result:
         remove_inter_result(output)
+    print("Pipeline finished successfully.")
